70ec92312a0f883e63264f07e36bf0b537988f24
Molly Ireland <molly.ireland@florey.edu.au>
2025-01-14 12:18:40 +1100

    Added functions
    1. _select_wm
    
    2. _get_mod
    
    3. percentile_enhanced
    
    4. _select_tissue

--- mriqc/workflows/utils.py
+++ mriqc/workflows/utils.py
@@ -245,3 +245,33 @@ def generate_filename(in_file, dirname=None, suffix='', extension=None):
         suffix = f'_{suffix}'
 
     return dirname / f'{stem}{suffix}{extension}'
+
+def _select_wm(inlist):
+    tissue = [f for f in inlist if 'WM' in f][0]
+    return tissue
+
+def _get_mod(in_file):
+    from pathlib import Path
+    return Path(in_file).name.rstrip('.gz').rstrip('.nii').split('_')[-1]
+
+def percentile_enhanced(in_arr, tissue_arr, percentile):
+    import numpy as np
+    range_max = np.percentile(in_arr[in_arr > 0], percentile)       # 99.98th percentile non-zero voxel values to identify outliers
+    excess = in_arr > range_max                                     #binary mask to identify voxels exceeding range_max
+    tissue_arr[excess] = 0                                          #voxels exceeding range_max map to zero in tissue probability to exclude outliers
+
+    tissue_mu = np.average(in_arr, weights=tissue_arr)              # Calculate weighted mean and standard deviation
+    tissue_sigma = np.sqrt(np.average((in_arr - tissue_mu) ** 2, weights=tissue_arr))
+
+    #generates new values more representative of overall intensity distribution: assigns values from a normal dist  
+    in_arr[excess] = np.random.normal(loc=tissue_mu, scale=tissue_sigma, size=excess.sum())
+    return in_arr
+
+
+def _select_tissue(modality, inlist):
+    max_tissue = {'T1w'     : {'tissue':'WM', 'file': 'segment_03'},
+                  'T2w'     : {'tissue':'CSF', 'file': 'segment_01'},
+                  'FLAIR'   : {'tissue':'GM', 'file': 'segment_02'}}
+
+    tissue = max_tissue[modality]['tissue']
+    return [f for f in inlist if tissue in f][0]
